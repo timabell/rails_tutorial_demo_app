aa27f7018c46e0e4995ebb16fb259471cb35242d
Tim Abell <tim@timwise.co.uk>
2013-05-15 12:37:50 +0100

    blank rails app for chapter 2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '5e3028fdadfda9912f37809859de9d7e46a49e7ccb0494ec8e3cd027755ceaf6bd5730e8a4e5ab574269d17d9ffac8ff3baed0b177c78df2ef283ab94f2d9f11'
